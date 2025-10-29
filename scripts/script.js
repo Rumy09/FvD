36f807b85af9824e6ec37376e07f827c560b9c16
Rumy09 <100687498+Rumy09@users.noreply.github.com>
2025-10-29 15:43:45 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,24 @@
 // JavaScript Document
 console.log("hi");
+
+const plus = document.querySelector(".plus"),
+minus = document.querySelector(".minus"),
+number = document.querySelector(".number");
+
+let a = 1;
+
+plus.addEventListener("click",()=>{
+    a++;
+    a = (a <10) ? "0" + a : a;
+
+    number.innerText = a;
+});
+
+minus.addEventListener("click", ()=>{
+    if(a > 1){
+        a--;
+          a = (a <10) ? "0" + a : a;
+
+    number.innerText = a;
+    }
+})
